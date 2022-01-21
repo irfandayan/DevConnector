bdf1f3faa884b27c6c5f25bd0e7584804890455d
irfan dayan <engr.irfandayan@gmail.com>
2022-01-21 14:16:53 +0500

    Add middleware

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,12 +1,23 @@
-import { v4 as uuid } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 import { SET_ALERT, REMOVE_ALERT } from "./types";
 
-export const setAlert = (msg, alertType) => (dispatch) => {
-  const id = uuid.v4();
+// export const setAlert = (msg, alertType) => (dispatch) => {
+//   const id = uuidv4();
 
-  dispatch({
-    type: SET_ALERT,
-    payload: { msg, alertType, id },
-  });
+//   dispatch({
+//     type: SET_ALERT,
+//     payload: { msg, alertType, id },
+//   });
+// };
+
+export const setAlert = function (msg, alertType) {
+  return (dispatch) => {
+    const id = uuidv4();
+
+    dispatch({
+      type: SET_ALERT,
+      payload: { msg, alertType, id },
+    });
+  };
 };
